33322fc1ab9932abc8fb3dc262a0708fc608cf78
anshbkeai <ansh.badam@gmail.com>
2026-01-07 23:53:11 +0000

    Added the Consteruce to get the input parse

--- src/main/java/com/leaderboard/lead/DTO/ScoreRequestDTO.java
+++ src/main/java/com/leaderboard/lead/DTO/ScoreRequestDTO.java
@@ -1,10 +1,14 @@
 package com.leaderboard.lead.DTO;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ScoreRequestDTO {
 
     private String userId;
